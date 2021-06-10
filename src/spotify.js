2ee14af5e5f895baff427b0806f232756a98a11a
centek134 <centek134@wp.pl>
2021-06-10 20:00:06 +0200

    Now getToken function works

--- src/spotify.js
+++ src/spotify.js
@@ -14,3 +14,16 @@ const scopes = [
 export const loginUrl = `${endPointUrl}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
   "%20"
 )}&response_type=token&show_dialog=true`;
+
+export const getToken = () => {
+  return window.location.hash
+  .substring(1)
+  .split("&")
+  .reduce((initial, item) => {
+    let parts = item.split("=");
+    initial[parts[0]] = decodeURIComponent(parts[1]);
+
+    return initial;
+  }, {})
+  
+}
