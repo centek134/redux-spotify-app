ee8452c11333c19d48484448853c7bffb827cbd0
centek134 <centek134@wp.pl>
2021-06-10 19:59:10 +0200

    No we can pull spotify access token

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Login from './containers/Login';
+import { getToken } from './spotify';
 //import {loginUrl} from './spotify';
 
 function App() {
   
+  useEffect(() => {
+    const token = getToken();
+    console.log("mój token = " + token);
+    setTimeout(()=> console.log(token), 1000)
+  },[]);
+
   return (
     <div className="App">
       <Login></Login>
