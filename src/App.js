821aaceb36147c0764d37e7c86a67d7e8708f851
centek134 <centek134@wp.pl>
2021-06-13 11:04:05 +0200

    setting dispatch actions

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import React, { useEffect, useState } from "react";
 import Login from "./containers/Login";
 import { getToken } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
+import { useStateProviderValue } from "./StateProvider";
 //import {loginUrl} from './spotify';
 
 const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
-  const [userId, setUserId] = useState(null);
+  const [{}, dispatch] = useStateProviderValue();
 
   useEffect(() => {
     const hashURL = getToken();
@@ -20,23 +21,13 @@ function App() {
 
     if (token) {
       setToken(token);
-      console.log(token);
       spotify.setAccessToken(token);
 
       spotify.getMe().then((user) => {
-        setUserId(user.id);
         console.log(user);
       });
-      spotify.getUserPlaylists(userId).then(
-        function (data) {
-          console.log("User playlists", data);
-        },
-        function (err) {
-          console.error(err);
-        }
-      );
     }
-  }, [userId]);
+  }, []);
 
   return (
     <div className="App">
