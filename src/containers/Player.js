bf14e0c0374dae26bbface99a24c04f8389d5be9
centek134 <centek134@wp.pl>
2021-06-25 21:19:10 +0200

    changes

--- src/containers/Player.js
+++ src/containers/Player.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import Body from "../components/Body";
+import Footer from "../components/Footer";
+import styled from "styled-components";
 
-function Player() {
-    return (
-        <div>
-            
-        </div>
-    )
+const PlayerBody = styled.div`
+  display: flex;
+`;
+
+function Player({ spotify }) {
+  return (
+    <React.Fragment>
+      <PlayerBody>
+        <Sidebar></Sidebar>
+        <Body></Body>
+      </PlayerBody>
+      <Footer></Footer>
+    </React.Fragment>
+  );
 }
 
-export default Player
+export default Player;
