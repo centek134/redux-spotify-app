95fc3f73baf8db6484d6145ef58d0422fb35b6e5
centek134 <centek134@wp.pl>
2021-06-25 21:18:35 +0200

    changes in return statement

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -3,19 +3,31 @@ import styled from "styled-components";
 
 const Option = styled.div`
   height: 40px;
-  cursor: pointer;
+  cursor: default;
   color: gray;
+  transition: 200ms color ease-in;
+  display: flex;
+  align-items: center;
 
   &:hover {
     color: #ffffff;
   }
 `;
-const IconItem = styled.p`
+const IconItem = styled.h4`
+  margin-left: 10px;
 `;
+
+const WithoutIcon = styled.p`
+  margin-left: 10px;
+`;
+
+
+
 export const SidebarOption = ({ title, Icon }) => {
   return (
     <Option>
-      <IconItem>{title}</IconItem>
+      {Icon && <Icon></Icon>}
+      {Icon? <IconItem>{title}</IconItem> : <WithoutIcon>{title}</WithoutIcon>}
     </Option>
   );
 };
