b9e4e72195a85a17a53f1bf896687ad4bc554b3f
centek134 <centek134@wp.pl>
2021-06-27 08:56:24 +0200

    finished footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,9 @@ import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
 import SkipNextIcon from '@material-ui/icons/SkipNext';
 import ShuffleIcon from '@material-ui/icons/Shuffle';
 import RepeatIcon from '@material-ui/icons/Repeat';
+import { Grid, Slider } from '@material-ui/core';
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
 
 const Foot = styled.footer`
     position: fixed;
@@ -22,6 +25,17 @@ const Foot = styled.footer`
 const FootLeft = styled.div`
     height: 100%;
     flex: 0.3;
+    color: #ffffff;
+    display: flex;
+    align-items: center;
+    max-width: 400px;
+`;
+
+const AlbumImg = styled.img`
+    height: 60px;
+    width: 60px;
+    object-fit: contain;
+    margin-right: 20px;
 `;
 const FootCenter = styled.div`
     height: 100%;
@@ -32,31 +46,84 @@ const FootCenter = styled.div`
     align-items: center;
     justify-content: space-between;
     max-width: 300px;
+    @media only screen and (max-width:850px){
+        padding: 0 25px;
+    }
+    @media only screen and (max-width:550px){
+        padding: 0;
+    }
 `;
 const FootRight = styled.div`
     height: 100%;
     flex: 0.3;
+    color: #ffffff;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 `;
+
+const AuthorName = styled.h4`
+    @media only screen and (max-width:850px){
+        font-size: 14px;
+    }
+`;
+const AlbumName = styled.p`
+    @media only screen and (max-width:850px){
+        font-size: 10px;
+    }
+`;
+
+const HoverStyles = styled.span`
+    margin: 0 5px;
+    &:hover{
+        transition: transform 0.2s ease-in-out;
+        transform: scale(1.2) !important;
+    }
+`
 
 const Footer = () => {
     return (
         <Foot>
             <FootLeft>
-                <p>Album</p>
+                <AlbumImg src = "https://www.thewrap.com/wp-content/uploads/2016/08/Rick-Astley-618x400.jpg" alt = ""/>
+                <div>
+                    <AuthorName>Rick Astley</AuthorName>
+                    <AlbumName>Never Gonna Give You Up</AlbumName>
+                </div>
             </FootLeft>
 
 
             <FootCenter>
-                <ShuffleIcon/>
-                <SkipPreviousIcon/>
-                <PlayCircleOutlineIcon/>
-                <SkipNextIcon/>
-                <RepeatIcon/>
+                <HoverStyles>
+                    <ShuffleIcon style = {{color:"green"}}/>
+                </HoverStyles>
+                <HoverStyles>
+                    <SkipPreviousIcon/>
+                </HoverStyles>
+                <HoverStyles>
+                    <PlayCircleOutlineIcon style = {{fontSize:"36px"}}/>
+                </HoverStyles>
+                <HoverStyles>
+                    <SkipNextIcon/>
+                </HoverStyles>
+                <HoverStyles>
+                    <RepeatIcon style = {{color:"green"}}/>
+                </HoverStyles>
             </FootCenter>
 
 
             <FootRight>
-                
+                <Grid container spacing = {2}>
+                    <Grid item>
+                        <PlaylistPlayIcon/>
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon/>
+                    </Grid>
+                    <Grid item xs>
+                        <Slider style = {{color:"green"}}/>
+                    </Grid>
+                </Grid>
             </FootRight>
 
         </Foot>
