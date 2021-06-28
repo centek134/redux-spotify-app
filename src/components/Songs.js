773c54b39df6d4965728d23221a7f23b4edd7f48
centek134 <centek134@wp.pl>
2021-06-28 13:36:34 +0200

    made component for song display

---
+++ src/components/Songs.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled from 'styled-components';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import { useStateProviderValue } from "../StateProvider";
+import SongRow from './SongRow';
+
+
+const SongsBody = styled.section`
+    margin: 20px -30px;
+`;
+const IconBody = styled.div`
+    display: flex;
+    align-items: center;
+    color: #ffffff;
+    & svg{
+        margin-right: 20px;
+    }
+    & svg.play_btn{
+        font-size: 80px !important;
+        margin: 20px 20px 20px 50px;
+        &:hover{
+            transition: 100ms transform ease-in;
+            transform: scale(1.08);
+        }
+    }
+`;
+export const Songs = () => {
+
+    const [{discover_weekly}, dispatch] = useStateProviderValue();
+
+    return (
+        <React.Fragment>
+            <SongsBody>
+
+            <IconBody>
+                <PlayCircleFilledIcon className = "play_btn"/>
+                <FavoriteIcon/>
+                <MoreHorizIcon/>
+            </IconBody>
+            {discover_weekly?.tracks.items.map( item => {
+                <SongRow track = {item.track} />
+            })}
+            </SongsBody>
+        </React.Fragment>
+    )
+}
