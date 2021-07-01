54be2ec316ee510ed9ada23f32f30b97b33cc142
centek134 <centek134@wp.pl>
2021-07-01 18:53:23 +0200

    Css changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,133 +1,125 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
-import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
-import SkipNextIcon from '@material-ui/icons/SkipNext';
-import ShuffleIcon from '@material-ui/icons/Shuffle';
-import RepeatIcon from '@material-ui/icons/Repeat';
-import { Grid, Slider } from '@material-ui/core';
-import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
-import VolumeDownIcon from '@material-ui/icons/VolumeDown';
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import { Grid, Slider } from "@material-ui/core";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
 
 const Foot = styled.footer`
-    position: fixed;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    bottom: 0;
-    width: 100%;
-    height: 70px;
-    padding: 15px;
-    background-color: #282828;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  bottom: 0;
+  width: 100%;
+  height: 70px;
+  padding: 15px;
+  background-color: #282828;
 `;
 
 const FootLeft = styled.div`
-    height: 100%;
-    flex: 0.3;
-    color: #ffffff;
-    display: flex;
-    align-items: center;
-    max-width: 400px;
-`;
+  height: 100%;
+  flex: 0.3;
+  color: #ffffff;
+  display: flex;
+  align-items: center;
+  max-width: 400px;
 
-const AlbumImg = styled.img`
+  & > img {
     height: 60px;
     width: 60px;
     object-fit: contain;
     margin-right: 20px;
+  }
+  @media only screen and (max-width: 850px) {
+    & > div > h4 {
+      font-size: 14px;
+    }
+    & > div > p {
+      font-size: 10px;
+    }
+  }
 `;
+
 const FootCenter = styled.div`
-    height: 100%;
-    flex: 0.4;
-    color: #ffffff;
-    padding: 0 100px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    max-width: 300px;
-    @media only screen and (max-width:850px){
-        padding: 0 25px;
+  height: 100%;
+  flex: 0.4;
+  color: #ffffff;
+  padding: 0 100px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 300px;
+
+  & > svg {
+    margin: 0 5px;
+    &:hover {
+      transition: transform 0.2s ease-in-out;
+      transform: scale(1.2) !important;
     }
-    @media only screen and (max-width:550px){
-        padding: 0;
-    }
+  }
+  & > svg.green {
+    color: green;
+  }
+
+  @media only screen and (max-width: 850px) {
+    padding: 0 25px;
+  }
+  @media only screen and (max-width: 550px) {
+    padding: 0;
+  }
 `;
 const FootRight = styled.div`
-    height: 100%;
-    flex: 0.3;
-    color: #ffffff;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  height: 100%;
+  flex: 0.3;
+  color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
-
-const AuthorName = styled.h4`
-    @media only screen and (max-width:850px){
-        font-size: 14px;
-    }
-`;
-const AlbumName = styled.p`
-    @media only screen and (max-width:850px){
-        font-size: 10px;
-    }
-`;
-
-const HoverStyles = styled.span`
-    margin: 0 5px;
-    &:hover{
-        transition: transform 0.2s ease-in-out;
-        transform: scale(1.2) !important;
-    }
-`
 
 const Footer = () => {
-    return (
-        <Foot>
-            <FootLeft>
-                <AlbumImg src = "https://www.thewrap.com/wp-content/uploads/2016/08/Rick-Astley-618x400.jpg" alt = ""/>
-                <div>
-                    <AuthorName>Rick Astley</AuthorName>
-                    <AlbumName>Never Gonna Give You Up</AlbumName>
-                </div>
-            </FootLeft>
+  return (
+    <Foot>
+      <FootLeft>
+        <img
+          src="https://www.thewrap.com/wp-content/uploads/2016/08/Rick-Astley-618x400.jpg"
+          alt=""
+        />
+        <div>
+          <h4>Rick Astley</h4>
+          <p>Never Gonna Give You Up</p>
+        </div>
+      </FootLeft>
 
+      <FootCenter>
+        <ShuffleIcon className="green" />
+        <SkipPreviousIcon />
+        <PlayCircleOutlineIcon style={{ fontSize: "36px" }} />
+        <SkipNextIcon />
+        <RepeatIcon className="green" />
+      </FootCenter>
 
-            <FootCenter>
-                <HoverStyles>
-                    <ShuffleIcon style = {{color:"green"}}/>
-                </HoverStyles>
-                <HoverStyles>
-                    <SkipPreviousIcon/>
-                </HoverStyles>
-                <HoverStyles>
-                    <PlayCircleOutlineIcon style = {{fontSize:"36px"}}/>
-                </HoverStyles>
-                <HoverStyles>
-                    <SkipNextIcon/>
-                </HoverStyles>
-                <HoverStyles>
-                    <RepeatIcon style = {{color:"green"}}/>
-                </HoverStyles>
-            </FootCenter>
-
-
-            <FootRight>
-                <Grid container spacing = {2}>
-                    <Grid item>
-                        <PlaylistPlayIcon/>
-                    </Grid>
-                    <Grid item>
-                        <VolumeDownIcon/>
-                    </Grid>
-                    <Grid item xs>
-                        <Slider style = {{color:"green"}}/>
-                    </Grid>
-                </Grid>
-            </FootRight>
-
-        </Foot>
-    )
+      <FootRight>
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider style={{ color: "green" }} />
+          </Grid>
+        </Grid>
+      </FootRight>
+    </Foot>
+  );
 };
 
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,52 +1,51 @@
-import React from 'react'
-import SearchIcon from '@material-ui/icons/Search';
-import  styled  from 'styled-components';
-import { Avatar } from '@material-ui/core';
-import { useStateProviderValue } from '../StateProvider';
+import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import styled from "styled-components";
+import { Avatar } from "@material-ui/core";
+import { useStateProviderValue } from "../StateProvider";
 
 const Head = styled.div`
-    color: #ffffff;
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 30px;
-`
+  color: #ffffff;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 30px;
+`;
 const HeadLeft = styled.div`
-    flex: 0.5;
-    background-color: #ffffff;
-    color: gray;
-    border-radius: 30px;
-    padding: 10px;
-    display: flex;
-    align-items: center;
-`
-const HeadRight = styled.div`
-    display: flex;
-    align-items: center;
-`
-const InputField = styled.input`
+  flex: 0.5;
+  background-color: #ffffff;
+  color: gray;
+  border-radius: 30px;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+
+  & > input {
     min-width: 60px;
     border: none;
     width: 100%;
     outline: none;
-`
-const AvatarName = styled.h4`
+  }
+`;
+const HeadRight = styled.div`
+  display: flex;
+  align-items: center;
+  & > h4 {
     margin-left: 10px;
-`
+  }
+`;
 
 export const Header = () => {
-
-    const [{user, dispatch}] = useStateProviderValue()
-    return (
-        <Head>
-            <HeadLeft>
-                <SearchIcon/>
-                <InputField placeholder = "search for a song of artists" />
-            </HeadLeft>            
-            <HeadRight>
-                <Avatar src = {user?.images[0]?.url} alt = "TO"/>
-                <AvatarName>{user?.display_name}</AvatarName>
-            </HeadRight>
-        
-        </Head>
-    )
+  const [{ user, dispatch }] = useStateProviderValue();
+  return (
+    <Head>
+      <HeadLeft>
+        <SearchIcon />
+        <input placeholder="search for a song of artists" />
+      </HeadLeft>
+      <HeadRight>
+        <Avatar src={user?.images[0]?.url} alt="TO" />
+        <h4>{user?.display_name}</h4>
+      </HeadRight>
+    </Head>
+  );
 };
